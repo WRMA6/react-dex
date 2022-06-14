94cb193e11bddbfe2f29e87bd7775b6edc39a657
Willard Ma <mawillard01@gmail.com>
2022-06-13 21:14:01 -0400

    Fix display for large screens

--- src/js/PokeList.jsx
+++ src/js/PokeList.jsx
@@ -5,7 +5,6 @@
 import React from 'react'
 import PokeThumbnail from './PokeThumbnail';
 
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 
 import { useSelector, } from 'react-redux'
@@ -22,15 +21,14 @@ function PokeList() {
 
   return (
     <div style={{ paddingTop: "2%" }}>
-      <Container maxWidth="md">
-        <Grid container spacing={{ xs: 3 }}>
-          {pokemons.map(p => (
-            <Grid item xs={4} sm={3} l={2} xl={1} key={p.name}>
-              <PokeThumbnail info={p} doTransition={p.id > numLoaded} />
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+      <Grid container spacing={{ xs: 3 }}
+        sx={{ paddingLeft: "5%", paddingRight: "5%" }}>
+        {pokemons.map(p => (
+          <Grid item xs={4} sm={3} lg={2} xl={1} key={p.name}>
+            <PokeThumbnail info={p} doTransition={p.id > numLoaded} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
